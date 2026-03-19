ad70f2cbe21f4dfa135e987c5e7dd24694f9cc44
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 22:36:22 -0300

    Adicionado o nome do Léo

--- main.c
+++ main.c
@@ -5,5 +5,7 @@ printf("Bem-vindo à FIAP"); //Incluso por Thor
 printf("Eu sou o Gabriel"); //Incluso por Gabriel Romão
 printf("Estamos fazendo a prova no grupo OTV"); // Incluso por Gabriel Romão
 printf("O RM do Thor é : 569543"); // Incluso por Gabriel Romão
+printf("Meu nome é Léo");// Incluso por Léo
+
 return 0;
 }
