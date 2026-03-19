49a4dca1912f3d9f4c5f558c03a6bb8b0933b862
GabrielRoma0 <gabrielbromao@gmail.com>
2026-03-18 22:29:17 -0300

    Adicionado nome e RM do Thor

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main (){
 printf("Bem-vindo à FIAP"); //Incluso por Thor
 printf("Eu sou o Gabriel"); //Incluso por Gabriel Romão
 printf("Estamos fazendo a prova no grupo OTV"); // Incluso por Gabriel Romão
+printf("O RM do Thor é : 569543"); // Incluso por Gabriel Romão
 return 0;
 }
