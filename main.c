0a832acfe3fd570645ccddf1285b0998616d4c84
GabrielRoma0 <gabrielbromao@gmail.com>
2026-03-18 22:27:26 -0300

    Adicionando novo print sobre a prova

--- main.c
+++ main.c
@@ -3,5 +3,6 @@
 int main (){
 printf("Bem-vindo à FIAP"); //Incluso por Thor
 printf("Eu sou o Gabriel"); //Incluso por Gabriel Romão
+printf("Estamos fazendo a prova no grupo OTV"); // Incluso por Gabriel Romão
 return 0;
 }
