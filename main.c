ba4a5febf08653f8ea96a3192463eee759d4934b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 22:42:43 -0300

    add rm fernando

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ printf("Estamos fazendo a prova no grupo OTV"); // Incluso por Gabriel Romão
 printf("O RM do Thor é : 569543"); // Incluso por Gabriel Romão
 printf("Meu nome é Léo");// Incluso por Léo
 printf("O RM do léo é: 569556"); // incluso por Léo
-print("Meu nome é Fernando"); // Incluso por Fernando
+printf("Meu nome é Fernando"); // Incluso por Fernando
+printf("O RM de Fernando é: 571408");  // Incluso por Fernando
 return 0;
 }
